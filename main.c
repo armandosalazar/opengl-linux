0349172f68498b17fbd7f32321963b73cfd7258c
Armando Salazar <armandosalazar351@gmail.com>
2023-06-22 05:33:08 +0000

    add null

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 
 int main(void) {
 	GLFWwindow* window;
+	// importante iniciar como NULL
+	GLFWwindow* w = NULL;
+
+	printf("%p\n", window);
+	printf("%p\n", w);
+
+	if (w == NULL)
+		printf("%s\n", "is null");
+
+	if (window == NULL)
+		printf("%s\n",  "null");
+
+	printf("%d\n", window == NULL);
+
 	return 0;
 }
