6c9ea1942727ac67eb546b921dde46c2a3522905
Armando Salazar <armandosalazar351@gmail.com>
2023-06-22 05:51:58 +0000

    draw triangle

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 `sudo pacman -S glfw-x11`
 
+*use glfw3*
+
 `gcc main.c -o main -lglfw`
+
+*use opengl*
+
+`gcc main.c -o main -lgldw -lGL`
 
--- main.c
+++ main.c
@@ -1,22 +1,47 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+void draw() {
+	glBegin(GL_TRIANGLES);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.0f, 0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glEnd();
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	draw();
+	glFlush();
+}
+
 int main(void) {
-	GLFWwindow* window;
-	// importante iniciar como NULL
-	GLFWwindow* w = NULL;
+	if (!glfwInit()) {
+		printf("Error: glfwInit failed\n");
+		return 1;
+	}
 
-	printf("%p\n", window);
-	printf("%p\n", w);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 
-	if (w == NULL)
-		printf("%s\n", "is null");
+	if (!window) {
+		printf("Error: glfwCreateWindow failed\n");
+		return 1;
+	}
 
-	if (window == NULL)
-		printf("%s\n",  "null");
+	glfwMakeContextCurrent(window);
 
-	printf("%d\n", sizeof(window));
-	printf("%d\n", sizeof(w));
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+		// Draw
+		display();
+
+		// Intercahnge buffers and poll IO events
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
