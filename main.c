960abdb539f819b301e86e8c481c541bfc52a655
Armando Salazar <armandosalazar351@gmail.com>
2023-06-22 21:47:10 -0600

    add bottom macos

--- main.c
+++ main.c
@@ -1,18 +1,64 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+
 #include "keys.h"
 
-// #include <arpa/inet.h>
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-// #define PORT 8080
-// #define BUFFER_SIZE 1024
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
 
-// typedef struct sockaddr_in sockaddr_in;
+int run_socker()
+{
+	int server_fd, new_socket, valread;
 
-// void run_socker() {
-// 	sockaddr_in address;
-// 	const char hello[] = "Hello from server";
-// }
+	sockaddr_in address;
+
+	int addrlen = sizeof(address);
+
+	char buffer[BUFFER_SIZE] = {0};
+	printf("%s\n", buffer);
+
+	const char hello[] = "Hello from server";
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		printf("Error: socket failed\n");
+		return 1;
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (sockaddr *)&address, sizeof(address)) < 0)
+	{
+		printf("Error: bind failed\n");
+		return 1;
+	}
+
+	if (listen(server_fd, 3) < 0)
+	{
+		printf("Error: listen failed\n");
+		return 1;
+	}
+
+	if ((new_socket = accept(server_fd, (sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+	{
+		printf("Error: accept failed\n");
+		return 1;
+	}
+
+	valread = read(new_socket, buffer, BUFFER_SIZE);
+	printf("%s\n", buffer);
+
+	send(new_socket, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+}
 
 void draw()
 {
